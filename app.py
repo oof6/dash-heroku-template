6329160cb1680e5b2f7adcc6251fb23f2103f5c7
oof6 <oof6@pitt.edu>
2025-12-11 17:03:31 -0500

    conf matrix

--- app.py
+++ app.py
@@ -547,7 +547,7 @@ Because our sample size is so small, creating bins was difficult as we needed en
                         #space
                         html.Div(style={'height': '20px'}),
 
-                        #html.Img(src='assets/knn_confusionMatrix.png', style={'width': '60%', 'height': 'auto'}),
+                        html.Img(src='https://raw.githubusercontent.com/oof6/dash-heroku-template/master/assets/knn_confusionMatrix.png', style={'width': '60%', 'height': 'auto'}),
 
                     
                         
