9d7e6812fc9696715cd1c446d78903bb7cae7714
oof6 <oof6@pitt.edu>
2025-12-12 08:24:13 -0500

    about page

--- app.py
+++ app.py
@@ -323,10 +323,7 @@ app.layout = html.Div(
                 dcc.Tab(label='About the Data', value='tab-1', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Div([
                         html.H3("About The Data", style={'color': theme['accent']}),
-
-                        
-                        html.Div([
-                            dcc.Markdown("""
+                        dcc.Markdown("""
                             ***What is the Golden Ratio?***
                                      
                                      
@@ -338,25 +335,20 @@ app.layout = html.Div(
                             ***Our Study***
                                      
 
-                            In the nature of facial aesthetics, the golden ratio is one of many frameworks for perceived attractiveness through balance and harmony in features. Although there are many different ratios that can be viewed and analyzed, in our study ,we focused on measurements between face length to width, eye spacing, and nose to mouth ratio. This is only a subset of the proportions deemed as “ideal”. 
+                            In the nature of facial aesthetics, the golden ratio is one of many frameworks for perceived attractiveness through balance and harmony in features. Although there are many different ratios that can be viewed and analyzed, in our study, we focused on measurements between face length to width, eye spacing, and nose to mouth ratio. This is only a subset of the proportions deemed as “ideal”. 
          
 
                            ***Our Data***
                                      
 
-                            We created our own dataset using data from our cohort. We collected information such as name, email, race, gender, and a headshot photo from 52 individuals within our cohort. We then used a convolutional neural network including python packages from opencv and mediapipe to build a complete dataset featuring measurements such as mouth width, nose width, face width, and face height. These features were then used to create face and mouth-nose ratios and averaged to create an overall “golden ratio score” or proportionality score. This complete dataframe is used for all of our models. 
+                            We created our own dataset using data from our cohort. We collected information such as name, email, race, gender, and a headshot photo from 52 individuals within our cohort. We then used a convolutional neural network to build a complete dataset featuring measurements such as mouth width, nose width, face width, and face height. These features were then used to create face and mouth-nose ratios and averaged to create an overall “golden ratio score” or proportionality score. This complete dataframe is used for all of our models. 
 
                             ***Complications and Caveats***
                                      
 
-                            Because of inconsistency in headshot files, posture, and quality of images, not all measurements from all individuals were able to be taken. In addition, small changes in posture and quality could affect our CNNs ability to correctly detect facial features. facial feature detection trial and error with using different networks was frustrating to say the least. OpenCV facial width and height originated as a square, eyebrows would be recognized as noses, background noise from images would be detected as faces themselves, or nothing would be detected at all. We went through over three different CNNs before settling on one and being satisfied that most faces were detected accurately. Moreover, we had a very small sample size, which most likely indicates that our analysis is not an accurate representation of the cohort. 
-
+                            Because of inconsistency in headshot files, posture, and quality of images, it was difficult to obtain measurements of everyone's faces when using packages like OpenCV. In addition, small changes in posture and quality could affect our CNNs ability to correctly detect facial features. Facial feature detection trial and error with using different networks was frustrating to say the least. We went through over three different CNNs before settling on one and being satisfied that most faces were detected accurately. This CNN was built by Google's Mediapipe package, which was pre-trained with thousands of faces beforehnad. Since our sample size was incredibly small, it was difficult for a model built from the ground up to accurately learn how to detect a face and facial features. Using MediaPipe allowed for a more accurate CNN for the etremely small sample size we had.
               
                             """, style={'color': theme['text']}),
-                        ],style = {'width':'48%', 'float':'left'}),
-                        html.Div([
-                            html.Img(src='https://raw.githubusercontent.com/oof6/dash-heroku-template/master/assets/Sophie_kim_photo.jpg', style = {'width':'48%', 'float':'right'}),
-                        ]),
                         
                         # Github Link
                         html.A(
@@ -365,8 +357,7 @@ app.layout = html.Div(
                             target="_blank"
                         )
                     ], style={'padding': '20px'})
-                ]),
-                
+                ]),                
                 # TAB 2
                 dcc.Tab(label='Dataset', value='tab-2', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Br(),
